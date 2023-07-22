f26afcfd02e87347b1a1a3183a4d911ce8482ce4
Moritz <moritz.huber@protonmail.com>
2023-07-22 21:42:33 +0200

    m

---
+++ exercises/quicksort/quicksort.c
@@ -0,0 +1,12 @@
+/*
+First just array, later random gen array, print, sort, print
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    int unsorted_array[] = {4, 19, 2, 384, 5, 28, 18};
+    printf("prints size of an integer %lu\n", sizeof(signed long));
+}
