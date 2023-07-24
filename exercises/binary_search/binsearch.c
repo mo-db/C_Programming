913f5d9758dd0bac9521eee6ad8c6abbf016b6bc
Moritz <moritz.huber@protonmail.com>
2023-07-24 20:58:12 +0200

    m

---
+++ exercises/binary_search/binsearch.c
@@ -0,0 +1,19 @@
+/*
+Goal of this program is to search a sorted array of either
+strings or numbers for matches.
+
+# Binary search in sorted char array
+
+
+# Binary search in sorted string array 
+Many issues due to lack of knowledge that need to be adressed:
+https://stackoverflow.com/questions/1088622/how-do-i-create-an-array-of-strings-in-c
+1. 
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+}
