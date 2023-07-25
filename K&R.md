bb570ee6734c68a78bca5c0c839feb61ff150d46
Moritz <moritz.huber@protonmail.com>
2023-07-25 22:52:25 +0200

    m

---
+++ K&R.md
@@ -0,0 +1,3 @@
+**macros**
+- Usually, it is recommended to use compile time constant variables
+    over #define. Because of scope and other reasons
--- exercises/src/fahrenheit.c
+++ exercises/src/fahrenheit.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int fahrenheit = 0;
-    int celsius;
+int main(void) {
+    /*
+    // My shity version
+
+    float fahrenheit = 0;
+    float celsius;
     printf("Fahrenheit Celsius\n");
     for (int n = 0; n < 16; n++)
     {
-        celsius = 5 * (fahrenheit-32) / 9;
-        printf("%d\t%d\n", fahrenheit, celsius);
+        celsius = (5.0/9.0) * (fahrenheit-32.0);
+        printf("%3.0f\t%6.1f\n", fahrenheit, celsius);
         fahrenheit += 20;
+    }
+    */
+
+
+   // for statement version
+   
+
+    int fahr;
+    for (fahr = 300; fahr >= 0; fahr -= 20) {
+        printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
     }
 }
