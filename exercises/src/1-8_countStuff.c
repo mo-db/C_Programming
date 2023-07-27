6de4dff6fc08968e6cc5ee9bc8b10547fd2187e9
Moritz <moritz.huber@protonmail.com>
2023-07-27 18:14:53 +0200

    x

---
+++ exercises/src/1-8_countStuff.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int c, blanks = 0, tabs = 0, new_lines = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') 
+            ++blanks;
+        if (c == '\t') 
+            ++tabs;
+        if (c == '\n')
+            ++new_lines;
+    }
+    
+    printf("Counted %d blanks, %d tabs and %d new lines", 
+        blanks, tabs, new_lines);
+
+}
---
+++ styleConvention.md
@@ -0,0 +1,17 @@
+# Files
+myCoolFile.c
+myCoolHeader.h
+
+1-4_myNumberedFile.c
+6-2_myNumberedHeader.h
+
+
+
+# Naming
+MY_DEFINE
+my_variable
+MyType
+
+myCoolFunction()
+myCoolFunction_special()
+
