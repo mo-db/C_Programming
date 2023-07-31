dd488c3c07abba0ac80b98b50ee96e7ac4e76af8
Moritz <moritz.huber@protonmail.com>
2023-07-31 17:13:31 +0200

    m

--- exercises/src/1-16-1_print_longest_line.c
+++ exercises/src/1-16-1_print_longest_line.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define MAXLINE 1000
 
-int getline(char current_line[], int limit);
+int get_line(char current_line[], int limit);
 void copy(char to[], char from[]);
 
 /* print longest input line */
@@ -12,19 +12,20 @@ int main(void)
     char current_line[MAXLINE];
     char longest_line[MAXLINE];
 
-    while ((current_length = getline(current_line, MAXLINE)) > 0) {
+    while ((current_length = get_line(current_line, MAXLINE)) > 0) {
         if (current_length > max_length) {
             max_length = current_length;
             copy(longest_line, current_line);
         }
     }
     if (max_length > 0) {
-        printf("%s", longest_line);
+        // %s expect a string that ends with \0
+        printf("printing longest line: %s", longest_line);
     }
     return 0;
 }
 
-int getline(char current_line[], int limit)
+int get_line(char current_line[], int limit)
 {
     int c, current_length = 0;
     for (int i = 0; i < limit - 1 && (c = getchar()) != EOF
