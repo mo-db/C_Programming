43cad3e78f4597c64998dd9387526c9fcfd70a1c
Moritz <moritz.huber@protonmail.com>
2023-08-01 17:17:51 +0200

    m

--- exercises/src/1-20_detab.c
+++ exercises/src/1-20_detab.c
@@ -1,11 +1,12 @@
 # include <stdio.h>
-# define TAB_SIZE 4
+# define TAB_SIZE 8
 
 int main(void)
 {
-    int c;
+    int c, n_blanks, pos;
     while ((c = getchar()) != EOF) {
         if (c == '\t') {
+            n_blanks = (TAB_SIZE - pos) % TAB_SIZE;
             int i;
             for (i = 0; i < TAB_SIZE; ++i)
                 putchar(' ');
