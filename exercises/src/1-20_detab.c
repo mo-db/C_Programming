e436e6eed8584c3d12e7b916407576c111273564
Moritz <moritz.huber@protonmail.com>
2023-08-03 10:46:11 +0200

    m

--- exercises/src/1-20_detab.c
+++ exercises/src/1-20_detab.c
@@ -1,17 +1,32 @@
+/*
+ * When the '\t' occurs
+ * 
+*/
+
 # include <stdio.h>
-# define TAB_SIZE 8
+# define TAB_SIZE 5
 
 int main(void)
 {
-    int c, n_blanks, pos;
+    printf("----|----|----|----|----|----|----|----|\n");
+    int c, n_blanks, pos = 1;
     while ((c = getchar()) != EOF) {
         if (c == '\t') {
-            n_blanks = (TAB_SIZE - pos) % TAB_SIZE;
+            //printf("Bevore Tab: %d ", pos);
+            // printf("pos test 1 = %d", pos);
+            n_blanks = (1 + TAB_SIZE) - (pos % TAB_SIZE);
             int i;
-            for (i = 0; i < TAB_SIZE; ++i)
+            for (i = 0; i < n_blanks; ++i) {
                 putchar(' ');
+                ++pos;
+            }
+            //printf("Pos: %d, Blanks: %d\n", pos, n_blanks);
+        } else if (c == '\n'){
+            putchar(c);
+            pos = 0;
         } else {
             putchar(c);
+            ++pos;
         }
     }
 }
--- exercises/src/1-21_entab.c
+++ exercises/src/1-21_entab.c
@@ -1,23 +1,30 @@
+/*
+ * 
+ *
+ * 
+*/
+
 # include <stdio.h>
-# define TAB_SIZE 4
+# define TAB_SIZE 5
 
 int main(void)
 {
-    int c, nb, pos;
-    nb = 0;
-    pos = 1;
-
+    printf("----|----|----|----|----|----|----|----|\n");
+    int c, n_blanks, pos = 1;
     while ((c = getchar()) != EOF) {
         if (c == '\t') {
-            nb = TAB_SIZE - (pos - 1) % TAB_SIZE;
-            while (nb > 0) {
+            //printf("Bevore Tab: %d ", pos);
+            // printf("pos test 1 = %d", pos);
+            n_blanks = (1 + TAB_SIZE) - (pos % TAB_SIZE);
+            int i;
+            for (i = 0; i < n_blanks; ++i) {
                 putchar(' ');
                 ++pos;
-                --nb;
             }
-        } else if (c == '\n' ) {
+            //printf("Pos: %d, Blanks: %d\n", pos, n_blanks);
+        } else if (c == '\n'){
             putchar(c);
-            pos = 1;
+            pos = 0;
         } else {
             putchar(c);
             ++pos;
