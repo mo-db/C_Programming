33b73d9e59de4d90605c227ab540da1df0dc1f0d
Moritz <moritz.huber@protonmail.com>
2024-02-04 23:14:56 +0100

    m

--- exercises/src/4-3_polish_calculator.c
+++ exercises/src/4-3_polish_calculator.c
@@ -1,18 +1,82 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
 
-#include <stddef.h>
-//#include "io.h"
+#define OP_SIZE_MAX 100
+#define FOUND_NUMBER '0'
+
+int getop(char []);
+void push(double);
+double pop(void);
 
 
 int main()
 {
-    char test_char = 'a';
-    //printf("testchar value: %i", (int)test_char);
-    
-    int test_int = 97;
-    unsigned int b;
-    b = (unsigned int)test_int;
-    
-    printf("testint value: %u", b);
+    int type;
+    double op2;
+    char s[OP_SIZE_MAX];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+        case FOUND_NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - pop());
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0) {
+                push(pop() / op2);
+            } else {
+                printf("error: zero devisor\n");
+            }
+            break;
+        case '\n':
+            printf("\t%.8g\n", pop());
+            break;
+        default:
+            printf("error: unknown command %s\n", s);
+            break;
+        }
+    }
+
+    printf("test\n");
     return 0;
+}
+
+
+#define STACK_DEPTH_MAX 100
+
+int stack_position = 0;
+double stack[STACK_DEPTH_MAX];
+
+
+void push(double f)
+{
+    if (stack_position < STACK_DEPTH_MAX) {
+        stack[stack_position++] = f;
+    } else {
+        printf("error: stack full, can't push %g\n", f);
+    }
+}
+
+
+double pop(void)
+{
+    if (stack_position > 0) {
+        return stack[--stack_position];
+    } else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
 }
