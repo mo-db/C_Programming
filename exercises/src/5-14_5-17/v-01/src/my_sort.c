0d8431a6416c5f6bae6e0e79d83e08086575402f
Moritz <moritz.huber@protonmail.com>
2024-03-03 15:20:42 +0100

    m

--- exercises/src/5-14_5-17/v-01/src/my_sort.c
+++ exercises/src/5-14_5-17/v-01/src/my_sort.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 
-void my_qsort(int *);
-int comp_descending(int, int);
-int comp_ascending(int, int);
+#define MAX_LINES 5
+
+void my_qsort(void *[]);
+int comp_descending(int);
+int comp_ascending(int);
 void my_swap(int *, int *);
+
+char *lineptr[MAX_LINES];
 
 int main(int argc, char * argv[])
 {
     int sample_ary[] = { 4, 8};
-    my_qsort(sample_ary);
+    my_qsort((void **)lineptr);
     return 0;
 }
 
-void my_qsort(int *sample_ary)
+void my_qsort(char *sample_ary[])
 {
     if (0) {
-        comp_ascending(sample_ary[0], sample_ary[1]);
+        comp_ascending(1);
     } else {
-        comp_descending(sample_ary[0], sample_ary[1]);
+        comp_descending(1);
     }
 }
 
-int comp_ascending(int v1, int v2)
+int comp_ascending(int v1)
 {
+    printf("asc");
     return 1;
 }
 
-int comp_descending(int v1, int v2)
+int comp_descending(int v1)
 {
+    printf("desc");
     return 1;
-}
-
-void my_swap(int *v1, int *v2)
-{
-    int temp = *v1;
-    *v1 = *v2;
-    *v2 = temp;
 }
